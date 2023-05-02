abd4f16d2fc57f3a7e7b30899696714a235a6387
ricardoshiga <roppongi_forever@hotmail.com>
2023-05-02 14:45:52 -0300

    Primeiros passos de PHP

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe geral do PHP</title>
+</head>
+
+<body>
+    <h1>Experimentando PHP</h1>
+    <hr>
+
+    <?php
+    echo "Ola, PHP!";
+    echo "texte";
+
+    $escola = "Senac";
+
+    echo $escola;
+
+
+    ?>
+
+
+
+
+</body>
+
+</html>
