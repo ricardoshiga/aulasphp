dabfed4e62a462189fd6dbc912353bc4becf8a74
ricardoshiga <roppongi_forever@hotmail.com>
2023-05-02 17:21:47 -0300

    Arrays em PHP

---
+++ 03-arrays.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    <h1>Arrays (vetores ou matrizes)</h1>
+    <hr>
+
+    <h2>Array com índice numérico</h2>
+    <?php
+    // Declarando arrays como variáveis
+    $bandas = ["Skid Row", "Metallica", "GunsNRores"];
+    $cursos = array("HTML5", "PHP", "Design", "JS");
+
+    // Declarando arrays como constantes
+    const UNIDADES_SENAC = ["Penha", "Tatuape"];
+    define("UNIDADES_SESC", ["Belenzinho", "Bertioga"]);
+    
+    ?>
+
+    <h3>Saída de dados arrays</h3>
+    <ul>
+        <li>Banda que mais gosto: <?=$bandas[0]?></li>
+        <li>Assunto estudado: <?=$cursos[1]?></li>
+        <li>Estamos no SENAC <?=UNIDADES_SENAC[0]?></li>
+        <li>O Sesc é uma colônia de férias <?=UNIDADES_SESC[1]?></li>
+    </ul>
+
+
+    <h3>Array Associativo</h3>
+    <?php
+    /* Chaves associativas */
+    $livros = [
+        "titulo" => "Senhor dos Anéis",
+        "autor" => "J.J.R. Tolkien",
+        "ano" => 1954
+    ];
+    
+    ?>
+
+    <p>Livro: <?=$livro["titulo"]?></p>
+    <p>Escrito por: <?=$livro["autor"]?> em <?=$livro?["ano"]>.</p>
+
+
+</body>
+</html>
